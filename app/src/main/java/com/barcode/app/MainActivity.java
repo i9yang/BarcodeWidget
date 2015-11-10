72bce008b7e2f29c8acb5e9ab6f9df8789eb3258
140363 <140363@emart.com>
2015-11-10 11:33:06 +0900

    Barcode 카메라, 사진에서 읽어오는 기능 추가

--- app/src/main/java/com/barcode/app/MainActivity.java
+++ app/src/main/java/com/barcode/app/MainActivity.java
@@ -1,13 +1,13 @@
 package com.barcode.app;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.appwidget.AppWidgetManager;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.*;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
@@ -15,19 +15,123 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.barcode.app.util.BarcodeUtil;
+import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import io.fabric.sdk.android.Fabric;
 import org.apache.commons.lang3.StringUtils;
 
 public class MainActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Fabric.with(this, new Crashlytics());
 		setContentView(R.layout.main_activity);
 
+		init();
+	}
+
+	public void saveBarcodeNo(View v) {
+		EditText et = (EditText) findViewById(R.id.barcodeNo);
+		if (StringUtils.isNotEmpty(et.getText())) {
+			changeBarcode(et.getText().toString());
+		}
+	}
+
+	private final int PICK_FROM_GALLERY = 999;
+
+	public void scanBarcodeNoFromCamera(View v) {
+		IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+		scanIntegrator.setOrientationLocked(false);
+		scanIntegrator.initiateScan();
+	}
+
+	public void scanBarcodeNoFromPhoto(View v) {
+		Intent intent = new Intent();
+		intent.setType("image/-");
+		intent.setAction(Intent.ACTION_GET_CONTENT);
+		startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_FROM_GALLERY);
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		String barcode = "";
+
+		if (requestCode == PICK_FROM_GALLERY) {
+			if (resultCode == Activity.RESULT_OK) {
+				try {
+					Bitmap barCodeimg = BitmapFactory.decodeStream(getContentResolver().openInputStream(intent.getData()));
+					BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this).build();
+					Frame myFrame = new Frame.Builder().setBitmap(barCodeimg).build();
+
+					SparseArray<Barcode> barcodes = barcodeDetector.detect(myFrame);
+
+					if (barcodes.size() != 0) {
+						barcode = barcodes.valueAt(0).displayValue;
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		} else {
+			IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+			if (scanningResult != null) {
+				barcode = scanningResult.getContents();
+			}
+		}
+
+		if (StringUtils.isNotEmpty(barcode)) {
+			AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+			alertDialog.setTitle("바코드 교체?");
+			alertDialog.setMessage(barcode);
+
+			final String result = barcode;
+
+			alertDialog.setPositiveButton("네", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					changeBarcode(result);
+				}
+			});
+
+			alertDialog.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.cancel();
+				}
+			});
+
+			alertDialog.create();
+			alertDialog.show();
+		}
+	}
+
+	public void changeBarcode(String result) {
+		SharedPreferences sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+		SharedPreferences.Editor editor = sharedPref.edit();
+		editor.putString(getString(R.string.barcodeNo), result);
+		editor.commit();
+
+		Toast toast = Toast.makeText(getApplicationContext(), "Changed!", Toast.LENGTH_SHORT);
+		toast.setGravity(Gravity.CENTER, 0, 0);
+		toast.show();
+
+		AppWidgetManager mgr = AppWidgetManager.getInstance(this);
+		Intent update = new Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+		update.setClass(this, WidgetProvider.class);
+		update.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, mgr.getAppWidgetIds(new ComponentName(this, WidgetProvider.class)));
+		this.sendBroadcast(update);
+		init();
+	}
+
+	public void init() {
 		SharedPreferences sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
 		TextView tv = (TextView) findViewById(R.id.barcodeNoText);
 		ImageView iv = (ImageView) findViewById(R.id.barcodeImage);
 		String barcodeNo = sharedPref.getString(getString(R.string.barcodeNo), "");
+		EditText et = (EditText) findViewById(R.id.barcodeNo);
+		et.setText("");
 
 		try {
 			if (StringUtils.isNotEmpty(barcodeNo)) {
@@ -40,30 +144,7 @@ public class MainActivity extends Activity {
 		}
 	}
 
-	public void saveBarcodeNo(View v) {
-		EditText et = (EditText) findViewById(R.id.barcodeNo);
-		if (StringUtils.isNotEmpty(et.getText())) {
-			SharedPreferences sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-			SharedPreferences.Editor editor = sharedPref.edit();
-			editor.putString(getString(R.string.barcodeNo), et.getText().toString());
-			editor.commit();
-
-			TextView tv = (TextView) findViewById(R.id.barcodeNoText);
-			tv.setText(et.getText().toString());
-
-			Toast toast = Toast.makeText(getApplicationContext(), "Changed!", Toast.LENGTH_SHORT);
-			toast.setGravity(Gravity.CENTER, 0, 0);
-			toast.show();
-
-			AppWidgetManager mgr = AppWidgetManager.getInstance(this);
-			Intent update = new Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-			update.setClass(this, WidgetProvider.class);
-			update.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, mgr.getAppWidgetIds(new ComponentName(this, WidgetProvider.class)));
-			this.sendBroadcast(update);
-		}
-	}
-
 	public void forceCrash(View view) {
-        throw new RuntimeException("This is a crash");
-    }
+		throw new RuntimeException("This is a crash");
+	}
 }
