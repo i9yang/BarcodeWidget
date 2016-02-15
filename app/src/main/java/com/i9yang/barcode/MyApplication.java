f1f2e1f8fec9dd3c04c6a30eb460df1f5f6c56aa
140363 <140363@emart.com>
2016-02-15 14:21:07 +0900

    1회만 실행되게 수정

--- app/src/main/java/com/i9yang/barcode/MyApplication.java
+++ app/src/main/java/com/i9yang/barcode/MyApplication.java
@@ -4,15 +4,28 @@ import android.app.Application;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.RemoteViews;
 
-public class MyApplication extends Application{
+public class MyApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		AppWidgetManager mgr = AppWidgetManager.getInstance(this);
-		Intent update = new Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-		update.setClass(this, WidgetProvider.class);
-		update.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, mgr.getAppWidgetIds(new ComponentName(this, WidgetProvider.class)));
-		this.sendBroadcast(update);
+
+		SharedPreferences settings = getSharedPreferences("PREFS_NAME", 0);
+		boolean chkInstall = settings.getBoolean("FIRST_RUN", false);
+		if (!chkInstall) {
+		    AppWidgetManager mgr = AppWidgetManager.getInstance(this);
+			Intent update = new Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+			update.setClass(this, WidgetProvider.class);
+
+			update.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, mgr.getAppWidgetIds(new ComponentName(this, WidgetProvider.class)));
+			this.sendBroadcast(update);
+
+			settings = getSharedPreferences("PREFS_NAME", 0);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putBoolean("FIRST_RUN", true);
+            editor.commit();
+		}
 	}
 }
